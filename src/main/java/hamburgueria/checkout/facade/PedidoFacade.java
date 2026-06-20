d99a039221b4e1726f7fd0563c488ffb9e646c6d
Igor G. Rodrigues <igorgabriel0155@gmail.com>
2026-06-20 16:36:22 -0300

    Adicionar classe PedidoFacade para gerenciamento do processo de finalização de pedidos

---
+++ src/main/java/hamburgueria/checkout/facade/PedidoFacade.java
@@ -0,0 +1,45 @@
+package hamburgueria.checkout.facade;
+
+import hamburgueria.carrinho.CarrinhoCompras;
+import hamburgueria.catalogo.factory.IHamburguer;
+import hamburgueria.cozinha.factorymethod.TipoHamburguer;
+import hamburgueria.cozinha.singleton.EventBus;
+import hamburgueria.cozinha.singleton.EventoPedidoPago;
+import hamburgueria.cozinha.state.PedidoCozinha;
+import hamburgueria.financeiro.bridge.MetodoPagamentoAbstraction;
+import hamburgueria.financeiro.proxy.IGatewayPagamento;
+import hamburgueria.financeiro.strategy.IEstrategiaPrecificacao;
+
+public class PedidoFacade {
+    public boolean finalizarPedido(CarrinhoCompras carrinho,
+            IEstrategiaPrecificacao estrategia,
+            IGatewayPagamento gateway,
+            MetodoPagamentoAbstraction metodo) {
+
+        if (carrinho == null || carrinho.getItens().isEmpty()) {
+            throw new IllegalStateException("O carrinho está vazio.");
+        }
+
+        if (gateway == null) {
+            throw new IllegalArgumentException("Gateway não configurado.");
+        }
+
+        double subtotal = carrinho.getSubtotal();
+        double valorFinal = estrategia.calcularTotal(subtotal);
+        boolean pago = gateway.cobrar(metodo, valorFinal);
+
+        if (pago) {
+            PedidoCozinha pedidoCozinha = new PedidoCozinha("PED-" + System.nanoTime());
+
+            // Repassa os tipos de alimento (Ponte do Grupo 3)
+            for (IHamburguer item : carrinho.getItens()) {
+                pedidoCozinha.adicionarAlimentoParaPreparo(new TipoHamburguer());
+            }
+
+            // PONTE DO GRUPO 4: Aciona o EventBus
+            EventBus.getInstancia().publicar(new EventoPedidoPago(pedidoCozinha));
+            carrinho.esvaziar();
+        }
+        return pago;
+    }
+}
